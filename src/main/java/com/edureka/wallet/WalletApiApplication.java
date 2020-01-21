bc166decdb60dff5d7cd80154313df6841e9e950
John Dev1 <JDev1@dev1.org>
2020-01-21 10:46:15 -0500

    adding comments to walletapi

--- src/main/java/com/edureka/wallet/WalletApiApplication.java
+++ src/main/java/com/edureka/wallet/WalletApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WalletApiApplication {
-
+/** adding a comment**/
 	public static void main(String[] args) {
 		SpringApplication.run(WalletApiApplication.class, args);
 	}
--- src/test/java/com/edureka/wallet/walletapi/api/WalletService.java
+++ src/test/java/com/edureka/wallet/walletapi/api/WalletService.java
@@ -7,7 +7,7 @@ import com.edureka.wallet.walletapi.model.WalletDto;
 public interface WalletService {
 
 	WalletDto doTransaction(PaymentTransactionDto transaction);
-
+  //this is to demonstrate feature batches, release and merge
 	WalletDto getBalance(String emailId);
 
 }
